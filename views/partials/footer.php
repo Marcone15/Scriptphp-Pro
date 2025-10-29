e33e8428d50b44a112730bb8055f4969fe72784a
Marcone15 <fpauloan@gmail.com>
2025-10-29 18:49:54 -0300

    Zerando tudo

--- views/partials/footer.php
+++
@@ -1,7 +0,0 @@
-        <footer>
-            <div class="container-footer">
-                <p><?php echo date('Y'); ?> <?php echo htmlspecialchars($settings['name_website']); ?>© - Todos os direitos reservados.</p>
-            </div>
-        </footer>
-    </body>
-</html>
