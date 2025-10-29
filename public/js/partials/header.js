0ea997fb793d12d5c06f4899f57ccfaa86460e7e
Marcone15 <fpauloan@gmail.com>
2025-10-29 19:31:19 -0300

    Zerando tudo

--- public/js/partials/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const titleHeadElement = document.querySelector('.title-head');
-    const pageTitle = document.querySelector('title');
-
-    if (titleHeadElement && pageTitle) {
-        pageTitle.textContent = pageTitle.textContent.replace('*', titleHeadElement.textContent);
-    }
-});
--- views/partials/socials.php
+++
@@ -1,7 +0,0 @@
-<div class="share-buttons" style="padding-top: 7px;">
-    <button class="btn-share facebook" onclick="share('facebook')"><i class="bi bi-facebook"></i></button>
-    <button class="btn-share telegram" onclick="share('telegram')"><i class="bi bi-telegram"></i></button>
-    <button class="btn-share twitter" onclick="share('twitter')"><i class="bi bi-twitter-x"></i></button>
-    <button class="btn-share whatsapp" onclick="share('whatsapp')"><i class="bi bi-whatsapp"></i></button>
-</div>
-<script src="../../public/js/partials/socials.js"></script>
